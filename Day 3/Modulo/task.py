5ea50cb117ef2d9b9f24fb3b67af8eea157a33d8
Neelaka <neelakanadeeshan171@gmail.com>
2025-05-30 23:49:52 +0530

    day3 complete

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,7 @@
+print(3%2)
+num = int(input("enter your number"))
+
+if num  % 2 == 0 :
+    print("even")
+else :
+    print("odd")
--- Day 3/Nesting and Elif/task.py
+++ Day 3/Nesting and Elif/task.py
@@ -3,5 +3,12 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster")
+    age = int(input("what is your age:"))
+    if  50> age >18 :
+            print("50 rupees ticket")
+    elif  age > 50:
+        print("100 rupees ticket")
+    else:
+            print("free")
 else:
     print("Sorry you have to grow taller before you can ride.")
