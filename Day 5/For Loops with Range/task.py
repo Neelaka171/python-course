b8451f45a554bf649216b6d767728902cee7ba91
Neelaka <neelakanadeeshan171@gmail.com>
2025-06-02 22:58:11 +0530

    day 5  done

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,9 @@
+# for number in range(1,11):
+#     print(number)
+# for number in range(1,11,2):
+    # print(number)
+sum = 0
+for number in range(1,101):
+    print(number)
+    sum += number
+print(sum)
--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,11 @@
 fruits = ["Apple", "Peach", "Pear"]
+
+for f in "Apple":
+     print(f)
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "pie")
+print("hello")
+
+
